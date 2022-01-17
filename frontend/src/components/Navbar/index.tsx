f8adfe783922b9eae22c247d01098895b37ef76e
Felipe Pina <fop.net@gmail.com>
2022-01-16 22:44:11 -0300

    NavBar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import { ReactComponent as GitHubIcon } from 'assets/img/github.svg';
+import './styles.css';
+
+function NavBar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>Movie Store</h1>
+                    <a href="https://github.com/fopnet/spring-react">
+                        <div className="dsmovie-nav-contact">
+                            <GitHubIcon />
+                            <p>/devsuperior</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default NavBar;
