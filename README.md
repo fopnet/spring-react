2cc590133956045c22f6c13a3dc1f7a33b15e9a8
Felipe Pina <fop.net@gmail.com>
2022-01-30 13:02:49 -0300

    First request

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## [Demo app](https://app-movie-store.netlify.app/) deployed in netlify.com
 
 https://app-movie-store.netlify.app/
+
+Requirements:
+JAVA 11
+Node -v  -> v16.13.2
+
+start  frontend
+
+> yarn start
--- nvm_init.sh
+++ nvm_init.sh
@@ -1,7 +1,6 @@
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$HOME/.nvm" &&
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" &&  # This loads nvm bash_completion
 source ~/.nvm/nvm.sh
 
 echo "Original node version v10.19.0"
